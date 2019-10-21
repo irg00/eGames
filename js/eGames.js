684263219ef3d125b441aebefc1d154bbe3c3db7
irg00 <irg00@iesemilidarder.com>
2019-10-21 16:22:10 +0200

    v1
    Version 1

---
+++ js/eGames.js
@@ -0,0 +1,39 @@
+// // class Game {
+// //
+// //   constructor(id, name, developer, release, genre, pegi) {
+// //     this.id = id;
+// //     this.name = name;
+// //     this.developer = developer;
+// //     this.release = release;
+// //     this.genre = genre;
+// //     this.pegi = pegi;
+// //   }
+// //
+// //   print(){
+// //     // Fer servir cometes especials
+// //     return `id:${this.id} - name:${this.name} - developer: ${this.developer} - genre: ${this.genre}`;
+// //   }
+// //
+// // }
+// //
+// // // Función para pintar la tabla de juegos
+// // function printTableGames(lGames){
+// //
+// // 	console.log("print:llista jocs");
+// // 	let bodyGameList = document.getElementById("content-games");
+// // 	bodyGameList.innerHTML="";
+// // 	lGames.forEach(function(value){
+// // 				let currentGame = value;
+// // 				bodyGameList.innerHTML+=`
+// // 						<tr>
+// // 						 <td scope="row">${currentGame.id}</td>
+// // 						 <td>${currentGame.name}</td>
+// // 						 <td>${currentGame.developer}</td>
+// // 						 <td>${currentGame.pegi}</td>
+// //              <td>${currentGame.genre}</td>
+// // 						</tr>
+// // 						`;
+// // 			}
+// // 	);
+//
+// }
